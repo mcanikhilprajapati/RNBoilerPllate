b33fed397dcd9ce9770870152aa7952b98923e78
urva <btpldeveloper@bvalvue.com>
2025-12-26 10:58:06 +0530

    drawer navigation setup

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../screens/Home.tsx';
 import Profile from '../screens/Profile.tsx';
 import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 const AppNavigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Profile" component={Profile} />
-      </Stack.Navigator>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Profile" component={Profile} />
+      </Drawer.Navigator>
+
+      {/*<Stack.Navigator>*/}
+      {/*  <Stack.Screen name="Home" component={Home} />*/}
+      {/*  <Stack.Screen name="Profile" component={Profile} />*/}
+      {/*</Stack.Navigator>*/}
     </NavigationContainer>
   );
 };
